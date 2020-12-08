579368ca6701a101eea02cce94b342898e3dad71
Cody Snell <codyjsnell@yahoo.com>
2020-12-08 12:31:58 -0600

    adds check auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,35 @@
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const SECRET = process.env.SECRET;
-
 
 module.exports = {
   signup,
-  login
+  login,
 };
 
 async function signup(req, res) {
   const user = new User(req.body);
   try {
     await user.save();
-    const token= createJWT(user)
-    res.json({token, user});
+    // Send back a JWT and the User
+    const token = createJWT(user);
+    res.json({ token, user });
   } catch (err) {
     // Probably a duplicate email
     res.status(400).json(err);
   }
 }
+
 async function login(req, res) {
   try {
-    const user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(401).json({err: 'bad credentials'});
-    user.comparePassword(req.body.pw, (err, isMatch) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(401).json({ err: "bad credentials" });
+    user.comparePassword(req.body.password, (err, isMatch) => {
       if (isMatch) {
         const token = createJWT(user);
-        res.json({token});
+        res.json({ token, user });
       } else {
-        return res.status(401).json({err: 'bad credentials'});
+        return res.status(401).json({ err: "bad credentials" });
       }
     });
   } catch (err) {
@@ -38,8 +39,8 @@ async function login(req, res) {
 
 function createJWT(user) {
   return jwt.sign(
-    {user}, // data payload
+    { user }, // data payload
     SECRET,
-    {expiresIn: '24h'}
+    { expiresIn: "60d" }
   );
 }
